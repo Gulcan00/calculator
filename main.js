43d070dfe95755a32daa3fac62e1634ada8cf5d8
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-09-18 20:10:21 +0300

    Add operation functions

---
+++ main.js
@@ -0,0 +1,32 @@
+//Operations
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function mul(num1, num2) {
+    return num1 * num2;
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
+
+//Variables
+let num1 = num2 = 0;
+let operator = '';
+
+function operate(operator, num1, num2) {
+    switch (operator)
+    {
+        case '+': return add(num1, num2);
+        case '-': return sub(num1, num2);
+        case '*': return mul(num1, num2);
+        case '/': return div(num1, num2);
+        default: return null;
+    }
+}
+
